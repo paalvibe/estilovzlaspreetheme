55cf3b2c278cb626a0f20e913fe1ad2ce78244db
Pal de Vibe <paal@kanobo.com>
2012-07-06 12:52:14 -0430

    hacking around upgrade to spree 1.1 issues

--- app/overrides/remove_account_summary.rb
+++ app/overrides/remove_account_summary.rb
@@ -1,3 +1,4 @@
 Deface::Override.new(:virtual_path => %q{spree/users/show},
                      :name => %q{remove_account_summary},
+                     :insert_top => '',
                      :remove => %q{[data-hook='account_summary']})
